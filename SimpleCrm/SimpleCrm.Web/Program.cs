f3bc94df4a55cc564ecaeb9b9cbdcf3c544380e9
becom1ng <becom1ng@users.noreply.github.com>
2023-08-21 15:58:43 -0500

    Cleaned tracked files now present in gitignore.

--- SimpleCrm/SimpleCrm.Web/Program.cs
+++ SimpleCrm/SimpleCrm.Web/Program.cs
@@ -5,23 +5,45 @@ namespace SimpleCrm.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // REGISTER SERVICES HERE
+            builder.Services.AddRazorPages();
             builder.Services.AddSingleton<IGreeter, ConfigurationGreeter>();
+
             var app = builder.Build();
 
-            ConfigurationManager configuration = builder.Configuration;
+            // REGISTER MIDDLEWARE HERE
 
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Use appsettings.Development.json for greeting. Step 004 of lesson.
+            //ConfigurationManager configuration = builder.Configuration;
             //var greeting = configuration["Greeting"];
-            var greeting = greeter.GetGreeting();
-            app.MapGet("/", () => greeting);
+            //app.MapGet("/", () => greeting);
+            
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+
+            var greeter = app.Services.GetService<IGreeter>();
+            app.MapGet("/", () => greeter.GetGreeting());
 
             app.Run();
         }
 
-        private readonly IGreeter _greeter;
+        //private readonly IGreeter _greeter;
 
-        public Program(IGreeter greeter) 
-        {
-            _greeter = greeter;
-        }
+        //public Program(IGreeter greeter)
+        //{
+        //    _greeter = greeter;
+        //}
     }
 }
