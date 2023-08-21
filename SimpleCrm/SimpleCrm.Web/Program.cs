f2992af6da25f9143b99ad501b250acc96eaa4c0
becom1ng <becom1ng@users.noreply.github.com>
2023-08-21 12:40:03 -0500

    Created IGreeter interface and implementation class.

--- SimpleCrm/SimpleCrm.Web/Program.cs
+++ SimpleCrm/SimpleCrm.Web/Program.cs
@@ -5,14 +5,23 @@ namespace SimpleCrm.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSingleton<IGreeter, ConfigurationGreeter>();
             var app = builder.Build();
 
             ConfigurationManager configuration = builder.Configuration;
 
-            var greeting = configuration["Greeting"];
+            //var greeting = configuration["Greeting"];
+            var greeting = greeter.GetGreeting();
             app.MapGet("/", () => greeting);
 
             app.Run();
+        }
+
+        private readonly IGreeter _greeter;
+
+        public Program(IGreeter greeter) 
+        {
+            _greeter = greeter;
         }
     }
 }
---
+++ SimpleCrm/SimpleCrm/ConfigurationGreeter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCrm
+{
+    public class ConfigurationGreeter : IGreeter
+    {
+        public string GetGreeting()
+        {
+            throw new NotImplementedException();
+            // ADD IMPLEMENTATION OF CONFIGURATION FROM HOMEWORK ASSIGNMENT
+        }
+    }
+}
