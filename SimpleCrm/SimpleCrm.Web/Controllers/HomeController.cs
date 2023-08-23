f2bcf3a369be1b7c60cd170da875d47402a7fb3d
becom1ng <becom1ng@users.noreply.github.com>
2023-08-23 12:08:06 -0500

    Step14

--- SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
@@ -5,16 +5,18 @@ namespace SimpleCrm.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(string id)
-        {
-            var model = new CustomerModel
-            {
-                Id = 1,
-                PhoneNumber = "555-555-1234",
-                FirstName = "John",
-                LastName = "Doe",
-            };
+        private ICustomerData _customerData; // (2) a class field to hold the needed dependency
 
+        // (1) the 'constructor' can INJECT data contracts it needs like this
+        public HomeController(ICustomerData customerData)
+        {
+            _customerData = customerData; // (3) store the injected dependency into a class field
+        }
+
+        public IActionResult Index()
+        {
+            // (4) now use the dependency...
+            var model = _customerData.GetAll();
             return View(model);
         }
     }
