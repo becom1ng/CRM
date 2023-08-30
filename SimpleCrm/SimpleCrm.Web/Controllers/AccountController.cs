96c68991d205e6f786e166853afc4dbe355c4186
becom1ng <becom1ng@users.noreply.github.com>
2023-08-30 12:01:10 -0500

    step35

--- SimpleCrm/SimpleCrm.Web/Controllers/AccountController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/AccountController.cs
@@ -1,30 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using SimpleCrm.Web.Models.Account;
 
 namespace SimpleCrm.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<CrmUser> userManager;
+        private readonly SignInManager<CrmUser> signInManager;
+
+        public AccountController(UserManager<CrmUser> userManager, SignInManager<CrmUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
 
-        [HttpPost, ValidateAntiForgeryToken()]
-        public IActionResult Register(RegisterUserViewModel model)
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var customer = new CrmUser
+                var user = new CrmUser
                 {
                     UserName = model.UserName,
+                    Email = model.UserName,
                     DisplayName = model.DisplayName,
-                    PasswordHash = model.Password,
                 };
-                // TODO: create the user record.
-                return Content("Success!");
-                //return RedirectToAction(nameof(Details), new { id = customer.Id });
+                var createresult = await userManager.CreateAsync(user, model.Password);
+                if (createresult.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else {
+                    foreach (var result in createresult.Errors)
+                    {
+                        ModelState.AddModelError("", result.Description);
+                    }
+                }
+                //TODO: Failed to create, return error messages
+                return View();
             };
             return View();
         }
