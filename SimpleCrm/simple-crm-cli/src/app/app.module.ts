49bf909add0cd02b8e9c045a82f37e640cff56ef
becom1ng <ryan42fbd@gmail.com>
2023-09-28 17:04:38 -0500

    launch resolveSourceMapLocations for polyfills error, refactor

--- SimpleCrm/simple-crm-cli/src/app/app.module.ts
+++ SimpleCrm/simple-crm-cli/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { CustomerModule } from './customer/customer.module';
 
 @NgModule({
