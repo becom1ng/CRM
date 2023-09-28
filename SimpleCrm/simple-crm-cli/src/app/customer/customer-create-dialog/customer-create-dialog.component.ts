8679e48a48fb316278bf25652cb21f994941aba3
becom1ng <ryan42fbd@gmail.com>
2023-09-28 14:28:31 -0500

    58c: basic dialog structure added to the add customer dialog

--- SimpleCrm/simple-crm-cli/src/app/customer/customer-create-dialog/customer-create-dialog.component.ts
+++ SimpleCrm/simple-crm-cli/src/app/customer/customer-create-dialog/customer-create-dialog.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Customer } from '../customer.model';
 
 @Component({
   selector: 'crm-customer-create-dialog',
   templateUrl: './customer-create-dialog.component.html',
   styleUrls: ['./customer-create-dialog.component.scss']
 })
-export class CustomerCreateDialogComponent {
+export class CustomerCreateDialogComponent implements OnInit {
 
+  constructor(public dialogRef: MatDialogRef<CustomerCreateDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Customer | null
+  ) {}
+
+  // TODO: SAVE, get form data and return to parent component
+  save() {
+    const customer = {};
+    this.dialogRef.close(customer);  // pass in the data to give back to the parent, or nothing
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit(): void {}
 }
