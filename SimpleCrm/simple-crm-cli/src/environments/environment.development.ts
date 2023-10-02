dec907a62f97b191f8c788efbfe12d8387072dc3
becom1ng <ryan42fbd@gmail.com>
2023-10-02 10:55:23 -0500

    refactor

--- SimpleCrm/simple-crm-cli/src/environments/environment.development.ts
+++ SimpleCrm/simple-crm-cli/src/environments/environment.development.ts
@@ -1,6 +1,3 @@
-import { CustomerMockService } from "src/app/customer/customer-mock.service";
-import { CustomerService } from "src/app/customer/customer.service";
-
 export const environment = {
     production: false,
 };
--- SimpleCrm/simple-crm-cli/src/environments/environment.ts
+++ SimpleCrm/simple-crm-cli/src/environments/environment.ts
@@ -1,5 +1,3 @@
-import { CustomerService } from "src/app/customer/customer.service";
-
 export const environment = {
     production: true,
 };
