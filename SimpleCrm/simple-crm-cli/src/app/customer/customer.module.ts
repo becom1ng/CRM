46223c5e1a36a6a61c7cb668dc9375b7a8522136
becom1ng <ryan42fbd@gmail.com>
2023-10-03 11:32:31 -0500

    refactor

--- SimpleCrm/simple-crm-cli/src/app/customer/customer.module.ts
+++ SimpleCrm/simple-crm-cli/src/app/customer/customer.module.ts
@@ -4,22 +4,22 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { CustomerRoutingModule } from './customer-routing.module';
-import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CustomerService } from './customer.service';
-import { CustomerMockService } from './customer-mock.service';
-import { environment } from 'src/environments/environment';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { CustomerCreateDialogComponent } from './customer-create-dialog/customer-create-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { environment } from 'src/environments/environment';
+import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerService } from './customer.service';
+import { CustomerMockService } from './customer-mock.service';
 
+import { CustomerListPageComponent } from './customer-list-page/customer-list-page.component';
+import { CustomerCreateDialogComponent } from './customer-create-dialog/customer-create-dialog.component';
+import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 @NgModule({
   declarations: [
     CustomerListPageComponent,
