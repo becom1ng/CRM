4cef86e9b61f5ae960bcc174aa69f490d62c74a2
becom1ng <ryan42fbd@gmail.com>
2023-10-04 16:29:19 -0500

    CUSTOM_ELEMENTS_SCHEMA to replace appmodule import

--- SimpleCrm/simple-crm-cli/src/app/app.component.spec.ts
+++ SimpleCrm/simple-crm-cli/src/app/app.component.spec.ts
@@ -1,15 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
-      RouterTestingModule,
-      AppModule
-    ],
-    declarations: [AppComponent]
+      RouterTestingModule],
+    declarations: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA] // eliminates the need to import AppModule
   }));
   
   it('should create the app', () => {
@@ -44,7 +43,4 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('h4.navPipeline').textContent).toContain('Pipeline');
   });
-
-  // TODO:  Check for more content on the page.
-  
 });
