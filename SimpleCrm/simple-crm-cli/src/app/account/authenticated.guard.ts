669caa23b77169ad8b69cde26bf4b9b653b45912
becom1ng <becom1ng@users.noreply.github.com>
2023-11-14 12:08:12 -0600

    authguard cleanup

--- SimpleCrm/simple-crm-cli/src/app/account/authenticated.guard.ts
+++ SimpleCrm/simple-crm-cli/src/app/account/authenticated.guard.ts
@@ -1,24 +1,9 @@
 import { inject } from '@angular/core';
-import { map } from 'rxjs';
-
 import { Router } from '@angular/router';
-//import { AccountService } from './account.service';
 
 export const AuthenticatedGuard = () => {
-  const router = inject(Router);
-  //const accountService = inject(AccountService); // inject like this instead of in a constructor
+  const router = inject(Router); // inject like this instead of in a constructor
 
-  // then do a return of some value.  Same data types can be returned as in the older style (bool, UrlTree or observable of bool or UrlTree)
   console.log("Access allowed by AuthenticatedGuard.");
   return true;
-
-  // you can do more based on logged in user...
-  //return accountService.user.pipe(
-    // if getting data from a service to make a determination, pipe from that observable and add any 'operators' needed
-    //map(user => {
-      // TODO: check whatever you need to about the user
-      //return true; // these really return Observable<boolean> since its inside an operator
-    //})
-  //);
 }
-  
