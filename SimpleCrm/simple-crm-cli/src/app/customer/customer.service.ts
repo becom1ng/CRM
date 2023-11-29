bd7ce44194380233c77194d6ab595c23ddb4c967
becom1ng <becom1ng@users.noreply.github.com>
2023-11-29 12:50:07 -0600

    fix customer.service api url (to customerS)

--- SimpleCrm/simple-crm-cli/src/app/customer/customer.service.ts
+++ SimpleCrm/simple-crm-cli/src/app/customer/customer.service.ts
@@ -12,21 +12,21 @@ export class CustomerService {
   }
 
   search(term: string): Observable<Customer[]> {
-    return this.http.get<Customer[]>('/api/customer/search?term=' + term);
+    return this.http.get<Customer[]>('/api/customers/search?term=' + term);
   }
 
   get(customerId: number): Observable<Customer | undefined> {
-    return this.http.get<Customer>('/api/customer/' + customerId);
+    return this.http.get<Customer>('/api/customers/' + customerId);
   }
 
   insert(customer: Customer): Observable<Customer> {
-    return this.http.post<Customer>('/api/customer/save', customer);
+    return this.http.post<Customer>('/api/customers/save', customer);
   }
 
   update(customer: Customer): Observable<Customer> {
     // example url: /api/customer/5
     return this.http.put<Customer>(
-      `/api/customer/${customer.customerId}`,
+      `/api/customers/${customer.customerId}`,
       customer
     );
   }
