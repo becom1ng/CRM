571e107d9f5fabaff7676e53d9eb63924be1fb61
becom1ng <becom1ng@users.noreply.github.com>
2023-12-28 16:22:52 -0600

    161: table performance with trackBy

--- SimpleCrm/simple-crm-cli/src/app/customer/customer-list-table/customer-list-table.component.ts
+++ SimpleCrm/simple-crm-cli/src/app/customer/customer-list-table/customer-list-table.component.ts
@@ -1,18 +1,40 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { Customer } from '../customer.model';
 
 @Component({
   selector: 'crm-customer-list-table',
   templateUrl: './customer-list-table.component.html',
   styleUrls: ['./customer-list-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomerListTableComponent {
-  @Input() customers!: Customer[] | null;
+  @Input() customers!: Customer[];
   @Output() openCustomer = new EventEmitter<Customer>();
+
+  // the column names must match the matColumnDef names in the html
+  displayColumns = [
+    'statusIcon',
+    'name',
+    'phoneNumber',
+    'emailAddress',
+    'status',
+    'lastContactDate',
+    'openDetailButton',
+  ];
 
   constructor() {}
 
   openDetailClick(customer: Customer) {
     this.openCustomer.emit(customer);
+  }
+
+  trackByUserId(index: number, item: Customer) {
+    return item.customerId; // some unique value on the array item
   }
 }
