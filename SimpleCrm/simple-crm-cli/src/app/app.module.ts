7de74b99c3e48266c9ddbade54c72b9d8ae73ba4
becom1ng <becom1ng@users.noreply.github.com>
2024-01-03 10:21:56 -0600

    163: converted to a PWA

--- SimpleCrm/simple-crm-cli/src/app/app.module.ts
+++ SimpleCrm/simple-crm-cli/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -19,6 +19,7 @@ import { JwtInterceptor } from './account/jwt.interceptor';
 import { layoutFeatureKey, layoutReducer } from './store/layout.store';
 import { EffectsModule } from '@ngrx/effects';
 import { environment } from 'src/environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -42,6 +43,12 @@ import { environment } from 'src/environments/environment';
         })
       : [],
     EffectsModule.forRoot([]),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     AppIconsService,
